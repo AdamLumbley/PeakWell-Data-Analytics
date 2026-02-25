9fd73f9641f82eee09c07cd39584a3ff3124b4d3
AdamLumbley <adamwlumbley@gmail.com>
2026-02-24 19:22:17 -0700

    Replace old KPIs with updated PeakWell Executive Dashboard queries

--- PeakWell_KPIs.sql
+++ PeakWell_KPIs.sql
@@ -4,19 +4,23 @@
 SELECT SUM(revenue) AS total_revenue
 FROM order_items;
 
--- 2. Total Orders
-SELECT COUNT(*) AS total_orders
-FROM orders;
-
--- 3. Total Customers
-SELECT COUNT(DISTINCT customer_id) AS total_customers
-FROM customers;
-
--- 4. Churn Rate / Retention
-SELECT
-    ROUND(SUM(churn_flag)*100.0/COUNT(*),2) AS churn_rate_percent,
-    ROUND(100.0 - SUM(churn_flag)*100.0/COUNT(*),2) AS retention_rate_percent
+-- 2. AOV
+SELECT ROUND(SUM(revenue)/COUNT(DISTINCT order_id),2) AS avg_order_value
 FROM order_items;
+
+-- 3. New Customers / Customer Growth Rate
+SELECT 
+    DATE_TRUNC('month', first_order_date) AS month,
+    COUNT(DISTINCT customer_id) AS new_customers
+FROM orders
+GROUP BY month
+ORDER BY month;
+
+--4. Revenue per Customer
+SELECT 
+    ROUND(SUM(oi.revenue) / COUNT(DISTINCT o.customer_id), 2) AS revenue_per_customer
+FROM order_items oi
+JOIN orders o ON oi.order_id = o.order_id;
 
 -- 5. Top Product Categories
 SELECT p.category, SUM(oi.revenue) AS category_revenue
